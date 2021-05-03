b37d8fdc735e809cecdb8c8a2844ca3c5909568e
lobilex <81357360+lobilex@users.noreply.github.com>
2021-05-03 17:56:50 +0200

    aq

--- Assets/script/domandaUpdareDomande.cs
+++ Assets/script/domandaUpdareDomande.cs
@@ -8,10 +8,11 @@ public class domandaUpdareDomande : MonoBehaviour
     public domandaData primaDomanda;
     public domandaData RispostaA;
     public domandaData RispostaB;
+    public domandaData PTambiente;
     public Text TestoDomanda1;
     public Text TestoRispostaA;
     public Text TestoRispostaB;
-    public Text TestoEconomia;
+    public Text TestoAmbiente;
     public int economia;
     public int ambiente;
     public int salute;
@@ -22,6 +23,7 @@ public class domandaUpdareDomande : MonoBehaviour
         TestoDomanda1.text = primaDomanda.domanda1;
         TestoRispostaB.text = RispostaB.testoRispostaB1;
         TestoRispostaA.text = RispostaA.testoRispostaA1;
+        count++;
     }
     void Start()
     {
@@ -29,10 +31,6 @@ public class domandaUpdareDomande : MonoBehaviour
     }
     public void rispostaA()
     {
-        economia = economia + primaDomanda.variazioneEconomiaA;
-        salute = salute - primaDomanda.variazioneSaluteA;
-        ambiente = ambiente + primaDomanda.variazioneAmbienteA;
-        count++;
         if (count == 14)
         {
             Debug.Log("question limit reached");
@@ -44,95 +42,157 @@ public class domandaUpdareDomande : MonoBehaviour
             TestoRispostaB.text = RispostaB.testoRispostaB2;
             TestoRispostaA.text = RispostaA.testoRispostaA2;
 
-            economia = economia - 10;
-            salute = salute + 10;
-            ambiente = ambiente - 10;
+            economia = economia + primaDomanda.variazioneEconomiaA2;
+            salute = salute - primaDomanda.variazioneSaluteA2;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA2;
+            count++;
         }
         else if (count == 2)
         {
             TestoDomanda1.text = primaDomanda.domanda3;
             TestoRispostaB.text = RispostaB.testoRispostaB3;
             TestoRispostaA.text = RispostaA.testoRispostaA3;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 3)
         {
             TestoDomanda1.text = primaDomanda.domanda4;
             TestoRispostaB.text = RispostaB.testoRispostaB4;
             TestoRispostaA.text = RispostaA.testoRispostaA4;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 4)
         {
             TestoDomanda1.text = primaDomanda.domanda5;
             TestoRispostaB.text = RispostaB.testoRispostaB5;
             TestoRispostaA.text = RispostaA.testoRispostaA5;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 5)
         {
-            TestoDomanda1.text = primaDomanda.domanda7;
+            TestoDomanda1.text = primaDomanda.domanda6;
             TestoRispostaB.text = RispostaB.testoRispostaB6;
             TestoRispostaA.text = RispostaA.testoRispostaA6;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 6)
         {
             TestoDomanda1.text = primaDomanda.domanda7;
             TestoRispostaB.text = RispostaB.testoRispostaB7;
             TestoRispostaA.text = RispostaA.testoRispostaA7;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 7)
         {
             TestoDomanda1.text = primaDomanda.domanda8;
             TestoRispostaB.text = RispostaB.testoRispostaB8;
             TestoRispostaA.text = RispostaA.testoRispostaA8;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 8)
         {
             TestoDomanda1.text = primaDomanda.domanda9;
             TestoRispostaB.text = RispostaB.testoRispostaB9;
             TestoRispostaA.text = RispostaA.testoRispostaA9;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 9)
         {
             TestoDomanda1.text = primaDomanda.domanda10;
             TestoRispostaB.text = RispostaB.testoRispostaB10;
             TestoRispostaA.text = RispostaA.testoRispostaA10;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 10)
         {
             TestoDomanda1.text = primaDomanda.domanda11;
             TestoRispostaB.text = RispostaB.testoRispostaB11;
             TestoRispostaA.text = RispostaA.testoRispostaA11;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 11)
         {
             TestoDomanda1.text = primaDomanda.domanda12;
             TestoRispostaB.text = RispostaB.testoRispostaB12;
             TestoRispostaA.text = RispostaA.testoRispostaA12;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 12)
         {
             TestoDomanda1.text = primaDomanda.domanda13;
             TestoRispostaB.text = RispostaB.testoRispostaB13;
             TestoRispostaA.text = RispostaA.testoRispostaA13;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 13)
         {
             TestoDomanda1.text = primaDomanda.domanda14;
             TestoRispostaB.text = RispostaB.testoRispostaB14;
             TestoRispostaA.text = RispostaA.testoRispostaA14;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
         else if (count == 14)
         {
             TestoDomanda1.text = primaDomanda.domanda15;
             TestoRispostaB.text = RispostaB.testoRispostaB15;
             TestoRispostaA.text = RispostaA.testoRispostaA15;
+
+            economia = economia + primaDomanda.variazioneEconomiaA;
+            salute = salute - primaDomanda.variazioneSaluteA;
+            ambiente = ambiente + primaDomanda.variazioneAmbienteA;
+            count++;
         }
     }
     public void rispostaB()
     {
-        economia = economia - primaDomanda.variazioneEconomiaB;
-        salute = salute + primaDomanda.variazioneSaluteB;
-        ambiente = ambiente - primaDomanda.variazioneAmbienteB;
-        count++;
         if (count == 14)
         {
             Debug.Log("question limit reached");
@@ -143,88 +203,161 @@ public class domandaUpdareDomande : MonoBehaviour
             TestoDomanda1.text = primaDomanda.domanda2;
             TestoRispostaB.text = RispostaB.testoRispostaB2;
             TestoRispostaA.text = RispostaA.testoRispostaA2;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 2)
         {
             TestoDomanda1.text = primaDomanda.domanda3;
             TestoRispostaB.text = RispostaB.testoRispostaB3;
             TestoRispostaA.text = RispostaA.testoRispostaA3;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 3)
         {
             TestoDomanda1.text = primaDomanda.domanda4;
             TestoRispostaB.text = RispostaB.testoRispostaB4;
             TestoRispostaA.text = RispostaA.testoRispostaA4;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 4)
         {
             TestoDomanda1.text = primaDomanda.domanda5;
             TestoRispostaB.text = RispostaB.testoRispostaB5;
             TestoRispostaA.text = RispostaA.testoRispostaA5;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 5)
         {
-            TestoDomanda1.text = primaDomanda.domanda7;
+            TestoDomanda1.text = primaDomanda.domanda6;
             TestoRispostaB.text = RispostaB.testoRispostaB6;
             TestoRispostaA.text = RispostaA.testoRispostaA6;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 6)
         {
             TestoDomanda1.text = primaDomanda.domanda7;
             TestoRispostaB.text = RispostaB.testoRispostaB7;
             TestoRispostaA.text = RispostaA.testoRispostaA7;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 7)
         {
             TestoDomanda1.text = primaDomanda.domanda8;
             TestoRispostaB.text = RispostaB.testoRispostaB8;
             TestoRispostaA.text = RispostaA.testoRispostaA8;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 8)
         {
             TestoDomanda1.text = primaDomanda.domanda9;
             TestoRispostaB.text = RispostaB.testoRispostaB9;
             TestoRispostaA.text = RispostaA.testoRispostaA9;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 9)
         {
             TestoDomanda1.text = primaDomanda.domanda10;
             TestoRispostaB.text = RispostaB.testoRispostaB10;
             TestoRispostaA.text = RispostaA.testoRispostaA10;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 10)
         {
             TestoDomanda1.text = primaDomanda.domanda11;
             TestoRispostaB.text = RispostaB.testoRispostaB11;
             TestoRispostaA.text = RispostaA.testoRispostaA11;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 11)
         {
             TestoDomanda1.text = primaDomanda.domanda12;
             TestoRispostaB.text = RispostaB.testoRispostaB12;
             TestoRispostaA.text = RispostaA.testoRispostaA12;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 12)
         {
             TestoDomanda1.text = primaDomanda.domanda13;
             TestoRispostaB.text = RispostaB.testoRispostaB13;
             TestoRispostaA.text = RispostaA.testoRispostaA13;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 13)
         {
             TestoDomanda1.text = primaDomanda.domanda14;
             TestoRispostaB.text = RispostaB.testoRispostaB14;
             TestoRispostaA.text = RispostaA.testoRispostaA14;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
         else if (count == 14)
         {
             TestoDomanda1.text = primaDomanda.domanda15;
             TestoRispostaB.text = RispostaB.testoRispostaB15;
             TestoRispostaA.text = RispostaA.testoRispostaA15;
+
+            economia = economia - primaDomanda.variazioneEconomiaB;
+            salute = salute + primaDomanda.variazioneSaluteB;
+            ambiente = ambiente - primaDomanda.variazioneAmbienteB;
+            count++;
         }
     }
     public void var()
     {
-    
+        string Franco = economia.ToString();
+        TestoAmbiente.text = PTambiente.Franco;
+         Franco = economia.ToString();
+
     }
 }
